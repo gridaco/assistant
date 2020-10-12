dda1bf8f0da9fa76e8f61ff0afd0b61d50c07ce1
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-13 02:27:47 +0900

    added innershadow support. fixed non material textstyle exception.

--- example/sample_flutter_app/lib/component.dart
+++ example/sample_flutter_app/lib/component.dart
@@ -4,96 +4,35 @@ class Component extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Stack(
-                children: [
-                  Container(
-                    width: 23,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Color(
-                        0xffffc600,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        4,
-                      ),
-                    ),
-                  ),
-                  Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                    ),
-                  ),
-                ],
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Color(
+            0xffffb290,
+          ),
+          borderRadius: BorderRadius.circular(
+            24,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color(
+                0xffffff,
               ),
-              width: 23,
-              height: 20,
-            ),
-            SizedBox(
-              height: 1.6666666666666667,
-            ),
-            Text(
-              "low preview quality warning",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                fontFamily: "Roboto",
+              offset: Offset(
+                4,
+                4,
               ),
+              blurRadius: 24,
+              spreadRadius: -12,
             ),
-            SizedBox(
-              height: 1.6666666666666667,
-            ),
-            SizedBox(
-              child: Text(
-                "there are some unresolved linting warnings on design section, so the built code and preview functionality might not be production ready.",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: "Roboto",
-                ),
+            BoxShadow(
+              color: Color(
+                0x3fffd689,
               ),
-              width: 366,
-            ),
-            SizedBox(
-              height: 1.6666666666666667,
-            ),
-            Container(
-              child: Stack(
-                children: [
-                  Container(
-                    child: Container(
-                      width: 180,
-                      height: 44,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Color(
-                            0xff373737,
-                          ),
-                          width: 1,
-                        ),
-                        borderRadius: BorderRadius.circular(
-                          4,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ],
-              ),
-              width: 180,
-              height: 44,
+              blurRadius: 4,
             ),
           ],
         ),
-        width: 366,
       ),
     );
   }
