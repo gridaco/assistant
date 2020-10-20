8e34618ce66cb14b495f8ae01e121db3c3d26e1e
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-20 10:02:45 +0900

    updated package:lint

--- figma/src/lint/lint.ts
+++ figma/src/lint/lint.ts
@@ -1,6 +1,9 @@
 import { lintMissingConstraints } from "@reflect.bridged.xyz/linter/lib/structure/constraints.lint";
 import lintNamingConventions from "@reflect.bridged.xyz/linter/lib/naming"
 export function runLints(node: SceneNode) {
-    lintMissingConstraints(node)
+    const constraintsWarnings = lintMissingConstraints(node)
+    console.warn(constraintsWarnings)
+
     const namingFeedbacks = lintNamingConventions(node.name)
+    console.warn(namingFeedbacks)
 }
