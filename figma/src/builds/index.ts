db06d9b1a3c652c6555cb1ebdb2fbdb71b2220f8
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-15 19:58:53 +0900

    implementing routes.make

---
+++ figma/src/builds/index.ts
@@ -0,0 +1,21 @@
+import { ImageRepository } from "../assets-repository"
+
+export abstract class BuildProcess {
+    readonly id: string
+    readonly processes: Array<BuildProcess> = []
+    imageAssetRepository: ImageRepository
+
+    constructor() {
+        this.id = Date.now().toString()
+    }
+
+
+    registerProcess(process: BuildProcess) {
+        this.processes.push(process)
+    }
+
+}
+
+export class FlutterBuilder extends BuildProcess {
+
+}
