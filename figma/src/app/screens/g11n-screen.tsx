1f69433cae3b2215d960d8e16abfc41dc1812519
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-20 18:38:41 +0900

    added g11n screen to launch locally running console (dev)

---
+++ figma/src/app/screens/g11n-screen.tsx
@@ -0,0 +1,95 @@
+import { VanillaScreenTransport } from "@bridged.xyz/client-sdk/lib";
+import { upload } from "@bridged.xyz/client-sdk/lib/hosting";
+import Button from "@material-ui/core/Button";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import React from "react"
+import { TransportableImageRepository } from "../../assets-repository";
+import { ImageHostingRepository } from "../../assets-repository/hosting";
+import { EK_COMPUTE_STARTED, EK_IMAGE_ASSET_REPOSITORY_MAP, EK_VANILLA_TRANSPORT } from "../constants/ek.constant";
+
+interface State {
+    loading: boolean
+    vanilla: VanillaScreenTransport
+}
+
+export class GlobalizationScreen extends React.Component<any, State> {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: false,
+            vanilla: undefined,
+        }
+        this.startCloud = this.startCloud.bind(this)
+    }
+
+    componentDidMount() {
+        window.addEventListener("message", this.onMessage);
+    }
+
+    onMessage = (ev: MessageEvent) => {
+        const msg = ev.data.pluginMessage;
+        console.log('msg', msg)
+
+        switch (msg.type) {
+            case EK_COMPUTE_STARTED:
+                this.setState(() => {
+                    return {
+                        loading: true
+                    }
+                })
+
+            case EK_VANILLA_TRANSPORT:
+                this.setState(() => {
+                    return {
+                        loading: false,
+                        vanilla: msg.data as VanillaScreenTransport
+                    }
+                })
+                console.log('vanilla transport receiced from view', msg.data)
+                break;
+
+            case EK_IMAGE_ASSET_REPOSITORY_MAP:
+                const imageRepo = msg.data as TransportableImageRepository
+                ImageHostingRepository.setRepository(imageRepo)
+                break
+        }
+    }
+
+    async startCloud() {
+        const hosted = await ImageHostingRepository.hostImages()
+        console.log(hosted)
+
+        const replaced = JSON.stringify(this.state.vanilla, (key, value) => {
+            if (hosted[value]) {
+                return hosted[value]
+            } else {
+                return value
+            }
+        }, 2)
+
+        console.log(replaced)
+
+        const uploaded = await upload({
+            file: replaced,
+            name: 'translation.json'
+        })
+
+        open(`http://localhost:3000/globalization/?url=${uploaded.url}`)
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>globalization</p>
+                {
+                    this.state.loading ? <LinearProgress /> : <p>ready</p>
+                }
+                <pre>{this.state.vanilla ? JSON.stringify(this.state.vanilla, null, 4) : 'nothing to load'}</pre>
+                <Button onClick={this.startCloud}>edit on the cloud</Button>
+            </div>
+        )
+    }
+}
--- figma/src/vanilla/index.ts
+++ figma/src/vanilla/index.ts
@@ -1,40 +1,57 @@
+import { TransportLayer, VanilaElement, VanillaScreenTransport } from "@bridged.xyz/client-sdk/lib";
 import { ReflectSceneNode, ReflectSceneNodeType, ReflectTextNode } from "@bridged.xyz/design-sdk/lib/nodes";
 import { TextManifest, ImageManifest } from '@reflect.bridged.xyz/core/lib'
+import { ImageRepository } from "../assets-repository";
 
 
-/**
- * for transporting vanilla elements
- */
-interface VanillaScreenTransport {
-
-}
-
-
-
+const vanillaImageRepo = new ImageRepository('vanilla-image-repository')
 /**
  * makes vanilla output, which contains only text data, and all others as image.
  * @param node 
  */
-function makeVanilla(node: ReflectSceneNode) {
+export function makeVanilla(node: ReflectSceneNode): VanillaScreenTransport {
+
     const vanillaElements: Array<VanilaElement> = []
     // 1. loop through all children, if only text, make text manifest.
     // 2. if not a text, go deeper, 
     node.children.forEach(c => {
-        const elements = fetchElements(c)
+        const elements = fetchElements(c, {
+            x: node.absoluteX,
+            y: node.absoluteY
+        })
+        // console.log('elements', elements)
         vanillaElements.push(...elements)
     })
+
+    // console.log('vanilla', vanillaElements)
+
+    return {
+        id: node.id,
+        project: 'temp',
+        width: node.width,
+        height: node.height,
+        elements: vanillaElements,
+        repository: vanillaImageRepo
+    }
 }
 
 
-type VanilaElement = TextManifest | ImageManifest
 
-function fetchElements(node: ReflectSceneNode): Array<VanilaElement> {
+function fetchElements(node: ReflectSceneNode, anchor: { x: number, y: number }): Array<VanilaElement> {
+    // console.log(`fetching elements from ${node.toString()}`)
     if (node instanceof ReflectTextNode) {
         // todo -> make text manifest
         const textStyle = node.textStyle;
         textStyle.color = node.primaryColor
-        return [
-            {
+        return [{
+            id: node.id,
+            index: node.hierachyIndex,
+            x: relativePositionToAnchor(anchor.x, node.absoluteX),
+            y: relativePositionToAnchor(anchor.y, node.absoluteY),
+            type: 'text',
+            width: node.width,
+            height: node.height,
+            src: {
                 text: node.characters,
                 textAlign: node.textAlignHorizontal,
                 textAlignVertical: node.textAlignVertical,
@@ -43,16 +60,48 @@ function fetchElements(node: ReflectSceneNode): Array<VanilaElement> {
                 overflow: undefined,
                 maxLines: undefined
             }
+        }
+
         ]
     }
+    const elements: Array<VanilaElement> = []
     const containstext = containsText(node)
     if (containstext) {
-        const elements: Array<VanilaElement> = []
         node.children.forEach(element => {
-            elements.push(...fetchElements(element))
+            elements.push(...fetchElements(element, anchor))
         });
-        return elements
+    } else {
+
+        (figma.getNodeById(node.id) as SceneNode).exportAsync({
+            format: "PNG"
+        })
+
+        const image = vanillaImageRepo.addImage({
+            key: node.id,
+            hash: undefined
+        })
+
+        const other: TransportLayer<ImageManifest> = {
+            id: node.id,
+            index: node.hierachyIndex,
+            x: relativePositionToAnchor(anchor.x, node.absoluteX),
+            y: relativePositionToAnchor(anchor.y, node.absoluteY),
+            type: 'other',
+            width: node.width,
+            height: node.height,
+            src: {
+                src: image.url,
+                width: node.width,
+                height: node.height,
+            }
+        }
+        elements.push(other)
     }
+    return elements
+}
+
+function relativePositionToAnchor(anchor: number, absolute: number) {
+    return absolute - anchor
 }
 
 /**
