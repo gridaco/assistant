9dc32d46e4eedb7f33c6ecd845c5b3359e1e574a
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-07 17:01:01 +0900

    updated corner interpreter with reflect border radius typings

--- figma/src/flutter/interpreter/corner.interpret.ts
+++ figma/src/flutter/interpreter/corner.interpret.ts
@@ -1,20 +1,21 @@
 import { mixed, ReflectRectangleNode } from "@bridged.xyz/design-sdk/lib/nodes";
 import { BorderRadiusGeometry, Radius, BorderRadius } from "@bridged.xyz/flutter-builder/lib";
+import { interpretRadius } from "./radius.interpret";
 
 export function interpretRectCorner(rect: ReflectRectangleNode): BorderRadiusGeometry {
     let borderRadius: BorderRadiusGeometry
 
     // figma.mixed means lrtb values are mixed.
-    if (rect.cornerRadius == mixed) {
+    if (rect.cornerRadius.all === undefined) {
         borderRadius = BorderRadius.only({
-            topLeft: rect.topLeftRadius == 0 ? undefined : Radius.circular(rect.topLeftRadius),
-            topRight: rect.topRightRadius == 0 ? undefined : Radius.circular(rect.topRightRadius),
-            bottomLeft: rect.bottomLeftRadius == 0 ? undefined : Radius.circular(rect.bottomLeftRadius),
-            bottomRight: rect.bottomRightRadius == 0 ? undefined : Radius.circular(rect.bottomRightRadius),
+            topLeft: rect.cornerRadius.tl == 0 ? undefined : interpretRadius(rect.cornerRadius.tl),
+            topRight: rect.cornerRadius.tr == 0 ? undefined : interpretRadius(rect.cornerRadius.tr),
+            bottomLeft: rect.cornerRadius.bl == 0 ? undefined : interpretRadius(rect.cornerRadius.bl),
+            bottomRight: rect.cornerRadius.br == 0 ? undefined : interpretRadius(rect.cornerRadius.br),
         })
     } else {
-        if (rect.cornerRadius != 0) {
-            borderRadius = BorderRadius.circular(rect.cornerRadius)
+        if (rect.cornerRadius.all != 0) {
+            borderRadius = BorderRadius.circular(rect.cornerRadius.all as number)
         }
     }
 
