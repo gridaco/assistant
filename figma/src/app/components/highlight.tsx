090e7b65f388a7dcd2b5f36e6f7074953e33cc29
You-J <ijyk3061@gmail.com>
2020-12-29 22:52:20 +0900

    Stash add preview loading progress

--- figma/src/app/components/highlight.tsx
+++ figma/src/app/components/highlight.tsx
@@ -1,85 +1,112 @@
-import * as React from 'react';
-import { default as PrismHighlight, defaultProps, Language } from "prism-react-renderer";
-import copy from 'copy-to-clipboard';
-import "./highlight.css"
+import * as React from "react";
+import {
+  default as PrismHighlight,
+  defaultProps,
+  Language,
+} from "prism-react-renderer";
+import copy from "copy-to-clipboard";
+import "./highlight.css";
 
 // region custom dart support
 // https://github.com/FormidableLabs/prism-react-renderer/issues/22#issuecomment-553042928
-import Prism from 'prism-react-renderer/prism';
-import dartLang from 'refractor/lang/dart';
-import { EK_COPIED } from '../constants/ek.constant';
-import { quickLook } from '../../quicklook';
-import { Widget } from '@bridged.xyz/flutter-builder/lib';
-import { notify } from '@bridged.xyz/design-sdk/lib/figma';
-import Button from '@material-ui/core/Button';
+import Prism from "prism-react-renderer/prism";
+import dartLang from "refractor/lang/dart";
+import { EK_COPIED } from "../constants/ek.constant";
+import { quickLook } from "../../quicklook";
+import { Widget } from "@bridged.xyz/flutter-builder/lib";
+import { notify } from "@bridged.xyz/design-sdk/lib/figma";
+import Button from "@material-ui/core/Button";
 dartLang(Prism);
 // endregion
 
 interface State {
-    isLaunchingConsole: boolean
+  isLaunchingConsole: boolean;
 }
 
 interface Props {
-    language: Language | any;
-    app: string
-    widget: Widget
-    code: string
-};
+  language: Language | any;
+  app: string;
+  widget: Widget;
+  code: string;
+}
 
 export default class Highlight extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLaunchingConsole: false,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLaunchingConsole: false
-        }
-    }
+  onCopyClicked = (e) => {
+    copy(this.props.code);
+    parent.postMessage({ pluginMessage: { type: EK_COPIED } }, "*");
+  };
 
-    onCopyClicked = (e) => {
-        copy(this.props.code);
-        parent.postMessage({ pluginMessage: { type: EK_COPIED } }, '*')
-    }
+  onQuickLookClicked = (e) => {
+    const setLoadingState = (loading: boolean) => {
+      this.setState((p, s) => {
+        return {
+          isLaunchingConsole: loading,
+        };
+      });
+    };
 
-    onQuickLookClicked = (e) => {
-        const setLoadingState = (loading: boolean) => {
-            this.setState((p, s) => {
-                return {
-                    isLaunchingConsole: loading
-                }
-            })
-        }
+    setLoadingState(true);
+    quickLook("quicklook", this.props.app)
+      .then((r) => {
+        setLoadingState(false);
+        notify(parent, "quick look ready !");
+      })
+      .catch((e) => {
+        console.error(e);
+        setLoadingState(false);
+        notify(parent, "compile failed. view console for details.", 2);
+      });
+  };
 
-        setLoadingState(true)
-        quickLook('quicklook', this.props.app).then((r) => {
-            setLoadingState(false)
-            notify(parent, "quick look ready !")
-        }).catch((e) => {
-            console.error(e)
-            setLoadingState(false)
-            notify(parent, "compile failed. view console for details.", 2)
-        })
-    }
-
-    render() {
-        return <code>
-            <div>
-                <div style={{ height: "24px" }} />
-                <Button variant="outlined" size="medium" className="sticky-actions" onClick={this.onCopyClicked}>copy code</Button>
-                <Button variant="outlined" size="medium" className="sticky-actions" disabled={this.state.isLaunchingConsole} onClick={this.onQuickLookClicked}>{this.state.isLaunchingConsole ? 'launching..' : 'quick look'}</Button>
-            </div>
-            <PrismHighlight {...defaultProps} Prism={Prism} code={this.props.code} language={this.props.language}>
-                {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                    <pre className={className} style={style}>
-                        {tokens.map((line, i) => (
-                            <div {...getLineProps({ line, key: i })}>
-                                {line.map((token, key) => (
-                                    <span {...getTokenProps({ token, key })} />
-                                ))}
-                            </div>
-                        ))}
-                    </pre>
-                )}
-            </PrismHighlight>
-        </code>
-    }
+  render() {
+    return (
+      <code>
+        <div>
+          <div style={{ height: "24px" }} />
+          <Button
+            variant="outlined"
+            size="medium"
+            className="sticky-actions"
+            onClick={this.onCopyClicked}
+          >
+            copy code
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            className="sticky-actions"
+            disabled={this.state.isLaunchingConsole}
+            onClick={this.onQuickLookClicked}
+          >
+            {this.state.isLaunchingConsole ? "launching.." : "quick look"}
+          </Button>
+        </div>
+        <PrismHighlight
+          {...defaultProps}
+          Prism={Prism}
+          code={this.props.code}
+          language={this.props.language}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </PrismHighlight>
+      </code>
+    );
+  }
 }
--- figma/src/app/components/preview.tsx
+++ figma/src/app/components/preview.tsx
@@ -1,47 +1,74 @@
-import { Typography } from "@material-ui/core";
+import { Typography, CircularProgress, Fade } from "@material-ui/core";
 import * as React from "react";
-import "./preview.css"
+import "./preview.css";
+import { setInterval } from "timers";
 
 interface Props {
-    data: Uint8Array,
-    name: string,
-    type?: string
+  data: Uint8Array;
+  name: string;
+  type?: string;
 }
 
 interface State {
-    url: string
+  url: string;
+  isLoading: boolean;
 }
+export class Preview extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: null,
+      isLoading: false,
+    };
+  }
 
-export class Preview extends React.Component<Props, State>{
-    constructor(props) {
-        super(props);
-        this.state = {
-            url: null
-        }
+  componentDidMount() {}
+
+  get url(): string {
+    if (this.props.data) {
+      var blob = new Blob([this.props.data], { type: "image/png" });
+      var url = URL.createObjectURL(blob);
+      return url;
     }
+  }
 
-    componentDidMount() {
+  loadingBox = () => {
+    const t = setInterval(() => <div>jhihi</div>, 1000);
 
-    }
+    return t;
+  };
 
-    get url(): string {
-        if (this.props.data) {
-            var blob = new Blob([this.props.data], { 'type': 'image/png' });
-            var url = URL.createObjectURL(blob);
-            return url;
-        }
-    }
+  render() {
+    let render = this.url ? (
+      <img
+        className="render"
+        alt={this.props.name}
+        src={this.url}
+        width="100%"
+        height="200px"
+      />
+    ) : (
+      <div className="render">
+        <Typography variant="h4" className="rendering-notify">
+          select anything
+        </Typography>
+      </div>
+    );
+    let t = this.url ? (
+      <div className="preview-loading">
+        <CircularProgress />
+      </div>
+    ) : (
+      ""
+    );
 
-    render() {
-        let render = this.url ?
-            <img className="render" alt={this.props.name} src={this.url} width="100%" height="200px"></img> :
-            <div className="render">
-                <br /><br /><h4>select anything</h4><br /><br />
-            </div>
+    return (
+      <div className="preview">
+        <Typography variant="caption">{this.props.type}</Typography>
 
-        return <div className="preview">
-            <Typography variant="caption">{this.props.type}</Typography>
-            {render}
-        </div>
-    }
+        <div className="preview-loading">{this.loadingBox}</div>
+        {render}
+      </div>
+    );
+  }
 }
