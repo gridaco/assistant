dd399e1e64e6795d20a130295ba0052556592b00
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-31 01:10:29 +0900

    Update platform initiallizer from design-sdk

--- webdev/src/index.tsx
+++ webdev/src/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import App from "app/lib/main"
-import { initializeTargetPlatform, TargetPlatform } from "app/lib/platform"
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import App from "app/lib/main";
+import {
+  initializeTargetPlatform,
+  TargetPlatform,
+} from "@bridged.xyz/design-sdk/lib/platform";
 
-
-initializeTargetPlatform(TargetPlatform.webdev)
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+initializeTargetPlatform(TargetPlatform.webdev);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
