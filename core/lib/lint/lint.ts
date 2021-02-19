281c8f5a5ccc8669d4b8b31cc8d0d3643b0aabe2
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-19 09:03:41 +0900

    update by linter api changes

--- core/lib/lint/lint.ts
+++ core/lib/lint/lint.ts
@@ -1,30 +1,9 @@
-import { lintMissingConstraints } from "@reflect.bridged.xyz/linter/lib/structure.lint/constraints.lint";
-import lintNamingConventions from "@reflect.bridged.xyz/linter/lib/naming.lint"
-import { detect } from "@reflect.bridged.xyz/detection/lib"
-import { ReflectLintFeedback } from "@reflect.bridged.xyz/linter/lib/feedbacks";
 import { ReflectSceneNode } from "@bridged.xyz/design-sdk/lib/nodes";
+import { DefaultSeectionLintRunner } from "@reflect.bridged.xyz/linter/lib/linter";
+
 export function runLints(node: ReflectSceneNode) {
-    // reject if selected node is too big for it's size or child count.
-    if (node.width > 3000) {
-        throw 'node too big for processing.'
-    }
+  const linter = new DefaultSeectionLintRunner(node);
 
-    const feedbacks: Array<ReflectLintFeedback> = []
-
-    const constraintsWarnings = lintMissingConstraints(node)
-    if (Array.isArray(constraintsWarnings)) {
-        feedbacks.push(...(constraintsWarnings))
-    }
-
-    const namingFeedbacks = lintNamingConventions(node)
-    if (Array.isArray(namingFeedbacks)) {
-        feedbacks.push(...(namingFeedbacks))
-    }
-
-    // test
-    const detected = detect(node)
-    console.warn('detected', detected)
-    // test
-
-    return feedbacks
+  const feedbacks = linter.runLints();
+  return feedbacks;
 }
