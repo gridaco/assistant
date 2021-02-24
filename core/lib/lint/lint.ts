0d19ea3041103dccfe296a4d3a024ead90ac3792
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-25 02:58:16 +0900

    add: typyings for runLints function

--- core/lib/lint/lint.ts
+++ core/lib/lint/lint.ts
@@ -1,7 +1,10 @@
 import { ReflectSceneNode } from "@bridged.xyz/design-sdk/lib/nodes";
+import { ReflectLintFeedback } from "@reflect.bridged.xyz/linter/lib/feedbacks/feedback";
 import { DefaultSeectionLintRunner } from "@reflect.bridged.xyz/linter/lib/linter";
 
-export function runLints(node: ReflectSceneNode) {
+export function runLints(
+  node: ReflectSceneNode
+): ReadonlyArray<ReflectLintFeedback> {
   const linter = new DefaultSeectionLintRunner(node);
 
   const feedbacks = linter.runLints();
