b2a736cac04231c6973937e7f359287fd3bdf665
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-27 06:24:29 +0900

    init selection state management in app

---
+++ app/lib/utils/plugin-provider/plugin-app-sdk/states/current-node.ts
@@ -0,0 +1,23 @@
+import { atom, GetRecoilValue, selector } from "recoil";
+
+const ATOM_KEY_CURRENT_NODE_SELECTION_PRIMARY =
+  "state.selection.node.primary.id";
+const SELECT_KEY_CURRENT_NODE_SELECTION_PRIMARY =
+  "state.selection.node.primary";
+
+// current primary selected node's id
+export const currentlySelectedPrimaryNodeId = atom({
+  key: ATOM_KEY_CURRENT_NODE_SELECTION_PRIMARY,
+  default: "",
+});
+
+// current primary selected node
+export const currentlySelectedPrimaryNode = selector({
+  get: ({ get }) => {
+    const id = get(currentlySelectedPrimaryNodeId);
+    // TODO fetch node data via id
+    throw "not implemeted";
+    return "";
+  },
+  key: SELECT_KEY_CURRENT_NODE_SELECTION_PRIMARY,
+});
