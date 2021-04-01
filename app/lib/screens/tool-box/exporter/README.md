5d32b6207b773efeb41782acb28f5b0ae926ec4b
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-02 04:12:18 +0900

    init exporter page

---
+++ app/lib/screens/tool-box/exporter/README.md
@@ -0,0 +1,3 @@
+# UI Exporter
+
+- reflect json exporter - expors ui as a whole json tree
---
+++ app/lib/screens/tool-box/exporter/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ExporterPage() {
+  return <div>Exporter page</div>;
+}
