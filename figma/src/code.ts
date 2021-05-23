ccf3bce487c9588dcaf6d7cfda72eb2ba62fef41
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-23 20:40:53 +0900

    resizing featue implemented

--- figma/src/code.ts
+++ figma/src/code.ts
@@ -47,18 +47,34 @@ export let singleFigmaNodeSelection: SceneNode;
 export let targetNodeId: string;
 
 async function showUI() {
+  // communicates with ui.html
   // load plugin with confugured w/h
-  const rawWidth = await figma.clientStorage.getAsync("width");
-  const rawHeight = await figma.clientStorage.getAsync("height");
-  let width = 375;
-  if (rawWidth) {
-    width = parseInt(rawWidth);
-  }
-  let height = 812;
-  if (rawHeight) {
-    height = parseInt(rawHeight);
-  }
-  figma.showUI(__html__, { width: width, height: height });
+  // restore previous size
+
+  const MIN_WIDTH = 320;
+  const MIN_HEIGHT = 600;
+
+  let size: { w: number; h: number } = { w: 375, h: 812 };
+  try {
+    const savedsize: {
+      w: number;
+      h: number;
+    } = await figma.clientStorage.getAsync("size");
+    savedsize && (size = savedsize);
+  } catch (_) {}
+
+  figma.ui.onmessage = (msg) => {
+    switch (msg.type) {
+      case "resize":
+        const w = Math.max(MIN_WIDTH, msg.size.w);
+        const h = Math.max(MIN_HEIGHT, msg.size.h);
+        figma.ui.resize(w, h);
+        figma.clientStorage.setAsync("size", { w: w, h: h }).catch((err) => {}); // save size
+        break;
+    }
+  };
+
+  figma.showUI(__html__, { width: size.w, height: size.h });
 }
 
 function delay(ms: number) {
