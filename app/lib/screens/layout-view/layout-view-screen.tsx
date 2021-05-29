4fbab712555a3f02fb76fb12c6315d05775ca0e8
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-30 07:51:58 +0900

    no-throw

--- app/lib/screens/layout-view/layout-view-screen.tsx
+++ app/lib/screens/layout-view/layout-view-screen.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { PluginSdk } from "../../utils/plugin-provider/plugin-app-sdk";
 
 export function LayoutViewScreen() {
-  PluginSdk.selectedNodeId;
+  // PluginSdk.selectedNodeId;
   return <p>Layout view</p>;
 }
