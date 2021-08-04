8e3dfe3e4910fcecc9f068a1b2b2c65bfd5b93cc
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-04 19:13:05 +0900

    fully implemented user provided file id like input parse & store (not tested)

--- figma/src/platform-features/file-checksum/index.ts
+++ figma/src/platform-features/file-checksum/index.ts
@@ -1,11 +1,41 @@
 export * as store from "./store";
 import * as store from "./store";
-import { parseFileId, analyze } from "@design-sdk/figma-url";
+import {
+  parseFileId,
+  analyze,
+  FigmaFileOrNodeIdType,
+  FigmaUrlType,
+} from "@design-sdk/figma-url";
 
-function saveUserInput(input: string) {
-  parseFileId(input);
-  // check if url
-  // if not url, check if filekey (prevalidation with maxlength) // before, decode url param if encoded value.
-  // parse input
-  store.saveFileKey_userprovided("");
+function saveUserInput(input: string): boolean {
+  let fileid;
+  try {
+    const res = analyze(input);
+    switch (res) {
+      //
+      case FigmaFileOrNodeIdType.fileid:
+      case FigmaFileOrNodeIdType.maybe_fileid:
+        fileid = input;
+        break;
+      case FigmaUrlType.node: // node is fileid + nodeid, CAN be used.
+      case FigmaUrlType.file:
+        fileid = parseFileId(input);
+        break;
+      //
+      case FigmaFileOrNodeIdType.nodeid:
+      case FigmaFileOrNodeIdType.maybe_nodeid:
+      case FigmaUrlType.embed:
+      case FigmaUrlType.empty:
+        // these are not value that can be saved.
+        return false;
+    }
+
+    if (fileid) {
+      store.saveFileKey_userprovided(fileid);
+    } else {
+      return false;
+    }
+  } catch (_) {
+    return false;
+  }
 }
