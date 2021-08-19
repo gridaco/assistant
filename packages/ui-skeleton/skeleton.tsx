07634937f571dce23a5268966be56fe771d4d783
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-19 20:53:33 +0900

    .

--- packages/ui-skeleton/skeleton.tsx
+++ packages/ui-skeleton/skeleton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-import { Skeleton } from "@material-ui/lab";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 export function AppSkeleton() {
   return (
