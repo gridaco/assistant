8b5a2cd2138dddefc424df824f336a5b16977491
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-27 03:43:18 +0900

    fix figma native build

--- app/__plugin__init__.ts
+++ app/__plugin__init__.ts
@@ -1,4 +1,4 @@
 // DO NOT REMOVE THIS LINE
-import "../../pages/code/__plugin";
-import "../../pages/tool-box/data-mapper/__plugin";
+import "./lib/pages/code/__plugin";
+import "./lib/pages/tool-box/data-mapper/__plugin";
 import "@app/design-lint/__plugin";
--- figma/src/ui.tsx
+++ figma/src/ui.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "app/lib/main";
-import { TargetPlatform } from "app/lib/utils/plugin-init/init-target-platform";
+import { TargetPlatform } from "@plugin-sdk/core";
 import * as ReactDOM from "react-dom";
 
 ReactDOM.render(
