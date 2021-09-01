bdcf57a50102eebbb13a0c46afb7e93137eb5160
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-01 17:18:02 +0900

    update default prisma view theme to vs-dark

--- packages/ui-code-box/codebox.tsx
+++ packages/ui-code-box/codebox.tsx
@@ -1,14 +1,6 @@
-import * as React from "react";
-import {
-  default as PrismHighlight,
-  defaultProps,
-  Language,
-  Prism,
-} from "prism-react-renderer";
-// import Prism from "prism-react-renderer/prism";
-import { useEffect, useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
-import { MonacoEditor } from "./monaco-editor";
+import { MonacoEditor, PrismView } from "./editors";
 
 export type SourceInput = string | { raw: string };
 
@@ -30,21 +22,11 @@ export function CodeBox({
 }) {
   const raw = typeof code == "string" ? code : code.raw;
 
-  useEffect(() => {
-    if (language == "dart") {
-      // region custom dart support
-      // https://github.com/FormidableLabs/prism-react-renderer/issues/22#issuecomment-553042928
-      const dartLang = require("refractor/lang/dart");
-      dartLang(Prism);
-      // endregion
-    }
-  }, []);
-
   const Editor =
     editor == "monaco" ? (
       <MonacoEditor src={raw} />
     ) : (
-      <PrismCodehighlight src={raw} language={language} />
+      <PrismView src={raw} language={language} />
     );
 
   return (
@@ -68,39 +50,7 @@ export function CodeBox({
   );
 }
 
-function PrismCodehighlight(props: { src: string; language: any | Language }) {
-  return (
-    <PrismHighlight
-      {...defaultProps}
-      Prism={Prism}
-      code={props.src}
-      language={props.language}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <CodeInnerWrapper className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </CodeInnerWrapper>
-      )}
-    </PrismHighlight>
-  );
-}
-
 const CodeWrapper = styled.code`
   width: max-content;
   height: auto;
-`;
-
-const CodeInnerWrapper = styled.pre`
-  width: 100%;
-  /* height: 408px; */
-  margin: 0 8px;
-  /* margin: 0 -8px; */
-  padding: 8px;
-  /* overflow: scroll; */
 `;
