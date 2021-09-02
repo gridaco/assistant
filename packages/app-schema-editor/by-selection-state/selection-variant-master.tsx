7acc20ffe97158131a4a7c26d8707e6d29b104d8
You-j <ijyk3061@gmail.com>
2021-09-02 13:42:10 +0900

    stash add: type handle func for interface package

--- packages/app-schema-editor/by-selection-state/selection-variant-master.tsx
+++ packages/app-schema-editor/by-selection-state/selection-variant-master.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { nodes } from "@design-sdk/core";
-import { VariantPropertyParser } from "@design-sdk/figma/features/variant";
+import {
+  FigmaNumber,
+  FigmaVariantPropertyCompatType,
+  VariantPropertyParser,
+} from "@design-sdk/figma/features/variant";
 import { CodeBox } from "@ui/codebox";
 import {
   buildeExampleData,
@@ -17,6 +21,8 @@ import {
 
 export default function (props: { node: nodes.light.IReflectNodeReference }) {
   const master = props.node;
+  const regxNumberType = /(number)/;
+  const regxBooleanType = /(boolean)/;
 
   const parser = new VariantPropertyParser(master);
   const data_of_properties = parser.getData(master);
@@ -24,11 +30,31 @@ export default function (props: { node: nodes.light.IReflectNodeReference }) {
     case: NameCases.pascal,
   }).name;
 
+  function typeToString(_data: FigmaVariantPropertyCompatType): any {
+    if (typeof _data === "symbol") {
+      const isNumberType = regxNumberType.test(_data.toString());
+      const isBooleanType = regxBooleanType.test(_data.toString());
+
+      if (isNumberType) {
+        return "number";
+      } else if (isBooleanType) {
+        // will be change to boolean
+        // it just for interface package typo missing
+        return "bool";
+      }
+    } else if (_data.type) {
+      return _data.type;
+    } else {
+      return "any";
+    }
+  }
+
   const interfaceAttrs: InterfaceAttr[] = parser.properties.map((d) => {
+    const attrTypes = typeToString(d.type);
     const _contorl: InterfaceTypeOption = {
-      name: "name",
-      value: "string",
-      description: "type",
+      name: attrTypes,
+      value: attrTypes,
+      description: `type is ${attrTypes}`,
     };
     return {
       label: d.key,
