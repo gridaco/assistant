ebb5976b085d93436538b5aa4524991c41afd740
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-09 17:11:35 +0900

    add empty selection view

--- packages/app-schema-editor/by-selection-state/selection-none.tsx
+++ packages/app-schema-editor/by-selection-state/selection-none.tsx
@@ -1,9 +1,62 @@
 import React from "react";
+import { Docstring } from "@code-ui/docstring";
+import { CodeBox } from "@ui/codebox";
+import styled from "@emotion/styled";
 
 export default function () {
   return (
     <>
-      <h6 style={{ padding: "0 16px" }}>nothing selected</h6>
+      <div style={{ backgroundColor: "#1e1e1e", padding: 16 }}>
+        <Docstring
+          controls={[
+            { name: "Select a component from figma" },
+            {
+              name:
+                "Click one of master component / instance / variant or layer under listed kinds.",
+            },
+            {
+              tag: "@",
+              name: "lang",
+              template: "{{tag}}{{name}} {{lang.value}}",
+              options: [
+                { name: "ts", value: "typescript" },
+                { name: "dart", value: "dart" },
+              ],
+            },
+          ]}
+          expandableConfig={undefined}
+          onChange={function (field: string, value: any): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+        <DisabledOverlay>
+          <DummyInterfacePreview />
+        </DisabledOverlay>
+      </div>
     </>
   );
 }
+
+const __placeholder_dummy_interface_code =
+  //
+  `interface ComponentProps {
+  property1: type;
+  property2: type;
+  property3: type;
+}
+`;
+
+function DummyInterfacePreview() {
+  return (
+    <CodeBox
+      editor="prism"
+      code={__placeholder_dummy_interface_code}
+      language={"typescript"}
+    />
+  );
+}
+
+const DisabledOverlay = styled.div`
+  /* TODO: add overlay color */
+  /* color: #8c1d1d1d; */
+`;
