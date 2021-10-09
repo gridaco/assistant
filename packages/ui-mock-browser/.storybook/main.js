4ad55b6e35b5c38603573a81610015fa29f5b49e
You-j <ijyk3061@gmail.com>
2021-10-10 08:18:11 +0900

    add storybook root

--- packages/ui-mock-browser/.storybook/main.js
+++ packages/ui-mock-browser/.storybook/main.js
@@ -23,6 +23,8 @@ module.exports = {
   stories: [
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 
