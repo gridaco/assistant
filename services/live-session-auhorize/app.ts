86a16ce6b0de197393fce27d9be3abb867341e73
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-29 20:56:03 +0900

    add simple pusher auth route

--- services/live-session-auhorize/app.ts
+++ services/live-session-auhorize/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import cors from "cors";
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID as string,
+  key: process.env.PUSHER_APP_KEY as string,
+  secret: process.env.PUSHER_APP_SECRET as string,
+  cluster: process.env.PUSHER_APP_CLUSTER as string,
+  useTLS: true,
+});
 
 const app = express();
-
 app.use(express.json());
 app.use(
   cors({
@@ -10,6 +18,11 @@ app.use(
   })
 );
 
-app.post("/format", (req, res) => {});
+app.post("/pusher/auth", (req, res) => {
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
 
 export default app;
