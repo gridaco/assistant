0e659f4fa2866165eb8bcec64243bc8228d99630
Universe <woojoo@softmarshmallow.com>
2023-02-24 03:18:14 +0900

    update masonry view lib

--- packages/app-photo-loader/photo-loader.tsx
+++ packages/app-photo-loader/photo-loader.tsx
@@ -5,13 +5,7 @@
 
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import {
-  CellMeasurer,
-  CellMeasurerCache,
-  createMasonryCellPositioner,
-  Masonry,
-} from "react-virtualized";
-import ImageMeasurer from "react-virtualized-image-measurer";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { list, itemsWithSizes } from "./.test";
 
 // Array of images with captions
@@ -23,120 +17,31 @@ const noCacheList = list.map((item, index) => ({
   image: item.image + (item.image ? "?noCache=" + Math.random() : ""),
 }));
 
-const keyMapper = (item, index) => item.image || index;
-
-const columnWidth = 200;
-const defaultHeight = 250;
-const defaultWidth = columnWidth;
-
-// Default sizes help Masonry decide how many images to batch-measure
-const cache = new CellMeasurerCache({
-  defaultHeight,
-  defaultWidth,
-  fixedWidth: true,
-});
-
-// Our masonry layout will use 3 columns with a 10px gutter between
-const cellPositionerConfig = {
-  cellMeasurerCache: cache,
-  columnCount: 3,
-  columnWidth,
-  spacer: 10,
-};
-
-const cellPositioner = createMasonryCellPositioner(cellPositionerConfig);
-
-const MasonryComponent = ({ itemsWithSizes, setRef }) => {
-  const cellRenderer = ({ index, key, parent, style }) => {
-    const { item, size } = itemsWithSizes[index];
-    const height = columnWidth * (size.height / size.width) || defaultHeight;
-
-    return (
-      <CellMeasurer cache={cache} index={index} key={key} parent={parent}>
-        <div style={style}>
-          <div>{item.title}</div>
-          {item.image && (
-            <img
-              src={item.image}
-              alt={item.title}
-              style={{
-                height: height,
-                width: columnWidth,
-                display: "block",
-              }}
-            />
-          )}
-        </div>
-      </CellMeasurer>
-    );
-  };
-
-  return (
-    <Masonry
-      cellCount={itemsWithSizes.length}
-      cellMeasurerCache={cache}
-      cellPositioner={cellPositioner}
-      cellRenderer={cellRenderer}
-      height={800}
-      width={300}
-      keyMapper={keyMapper}
-      ref={setRef}
-    />
-  );
-};
-
 export function PhotoLoader() {
   const [state, setState] = useState({ images: noCacheList });
-
-  let masonryRef = null;
-
-  // this shows how to significantly change the input array, if items will be only appended this recalculation is not needed
-
-  const setMasonry = (node) => (masonryRef = node);
-
   return (
     <Wrapper>
-      <Inner>
-        <p>
-          HAS ERROR! in react-virtualized image measurer <br /> <br />
-          <b> error message</b> <br />
-          <i>
-            Critical dependency: require function is used in a way in which
-            dependencies cannot be statically extracted
-          </i>
-          <br />
-          <br />
-          react-virtualized-image-measurer{" "}
-          <a href="https://codesandbox.io/s/7y66p25qv6?file=/index.js">
-            codesandbox link
-          </a>
-        </p>
-
-        {/* <ImageMeasurer
-        items={state.images}
-        image={(item) => item.image}
-        keyMapper={keyMapper}
-        onError={(error, item, src) => {
-          console.error(
-            "Cannot load image",
-            src,
-            "for item",
-            item,
-            "error",
-            error
-          );
-        }}
-        defaultHeight={defaultHeight}
-        defaultWidth={defaultWidth}
-      >
-        {({ itemsWithSizes }) => (
-          <MasonryComponent
-            setRef={setMasonry}
-            itemsWithSizes={itemsWithSizes}
-          />
-        )}
-      </ImageMeasurer> */}
-      </Inner>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry>
+          {state.images.map((item, index) => (
+            <GridItem key={index}>
+              <img
+                style={{ width: "100%", height: "100%", display: "block" }}
+                src={item.image}
+                alt=""
+              />
+            </GridItem>
+          ))}
+          {/* {images.map((image, i) => (
+            <img
+              key={i}
+              src={image}
+              style={{ width: "100%", display: "block" }}
+              alt=""
+            />
+          ))} */}
+        </Masonry>
+      </ResponsiveMasonry>
     </Wrapper>
   );
 }
@@ -145,26 +50,6 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
-const Inner = styled.div`
-  // FIXME: body margin 8 + inner margin 8 = figma style margin 16
-  margin: 8px;
-`;
-
-const Gird = styled.div`
-  display: grid;
-  gap: 12px;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-columns: auto;
-`;
-
-const GridLongItem = styled.div`
+const GridItem = styled.figure`
   background: gray;
-  height: 300px;
 `;
-
-const GridItem = styled.div`
-  background: gray;
-  height: 200px;
-`;
-
-const ImageLoader = styled.div``;
