a9998514ce41d32293238cf2c5f8aaade4455400
Universe <woojoo@softmarshmallow.com>
2023-02-24 05:40:03 +0900

    add experimental openai text completion

---
+++ web/pages/api/generative/text.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const OPENAI_API_SECRET_KEY = process.env.OPENAI_API_SECRET_KEY;
+const configuration = new Configuration({
+  apiKey: OPENAI_API_SECRET_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const q = req.query.q as string;
+  const model = "text-davinci-003";
+
+  const prompt = `
+Create headlines for my design content:
+prompt: ${q}
+output:
+`;
+
+  const { data } = await openai.createCompletion({
+    prompt: prompt,
+    model: model,
+  });
+
+  const { choices } = data;
+
+  const texts = choices.map((c) => c.text);
+
+  switch (req.method) {
+    case "GET": {
+      res.json({
+        q: q,
+        texts: texts,
+        model: model,
+      });
+    }
+  }
+}
