bafc5d08e46f766aed43e6efcddf839845e2ddbd
softmarshmallow <woojoo@softmarshmallow.com>
2023-02-25 04:10:58 +0900

    update search input

--- packages/ui-core/search/search.tsx
+++ packages/ui-core/search/search.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Search } from "@material-ui/icons";
 import styled from "@emotion/styled";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 const LoadingIndicator = withStyles((theme) => ({
   root: {},
@@ -12,6 +12,7 @@ const LoadingIndicator = withStyles((theme) => ({
 }))(CircularProgress);
 
 export function SearchInput({
+  onClear,
   onChange,
   onEnter,
   loading = false,
@@ -19,6 +20,7 @@ export function SearchInput({
   disabled = false,
   readonly = false,
 }: {
+  onClear?: () => void;
   onChange?: (value: string) => void;
   onEnter?: () => void;
   loading?: boolean;
@@ -26,10 +28,14 @@ export function SearchInput({
   disabled?: boolean;
   readonly?: boolean;
 }) {
+  const [value, setValue] = React.useState<string>("");
+
+  const canclear = onClear && value;
+
   return (
     <SearchBarWrapper>
       {loading && <LoadingIndicator />}
-      {!loading && <Search style={{ fontSize: "20px" }} />}
+      {!loading && <MagnifyingGlassIcon className="search-icon" />}
       <Input
         readOnly={readonly}
         disabled={disabled}
@@ -38,9 +44,27 @@ export function SearchInput({
             onEnter?.();
           }
         }}
+        value={value}
         placeholder={placeholder}
-        onChange={(e) => onChange?.(e.target.value)}
+        onChange={(e) => {
+          setValue(e.target.value);
+          if (e.target.value === "") {
+            onClear?.();
+          }
+          onChange?.(e.target.value);
+        }}
       />
+      {canclear && (
+        <button
+          className="clear"
+          onClick={() => {
+            setValue("");
+            onClear?.();
+          }}
+        >
+          <Cross2Icon />
+        </button>
+      )}
     </SearchBarWrapper>
   );
 }
@@ -48,13 +72,36 @@ export function SearchInput({
 const SearchBarWrapper = styled.div`
   font-size: 14px;
   height: 55px;
-  padding: 8px;
+  padding: 8px 16px;
   display: flex;
   align-items: center;
 
-  svg {
+  .search-icon {
     margin: 10px 10px 10px 8px;
-    font-size: 20px;
+    width: 20px;
+    height: 20px;
+  }
+
+  .clear {
+    padding: 0;
+    border: none;
+    background: none;
+    outline: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 50%;
+    background: rgba(0, 0, 0, 0.6);
+    width: 16px;
+    height: 16px;
+
+    svg {
+      color: white;
+      width: 12px;
+      height: 12px;
+    }
   }
 `;
 
