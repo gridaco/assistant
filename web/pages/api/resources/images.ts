b30e1a41a35659b378c83700295838707862e728
Universe <woojoo@softmarshmallow.com>
2023-02-26 10:41:41 +0900

    add support for random images

--- web/pages/api/resources/images.ts
+++ web/pages/api/resources/images.ts
@@ -5,6 +5,24 @@ const unsplash = createApi({
   accessKey: process.env.UNSPLASH_API_CLIENT_ID,
 });
 
+type Photo = Awaited<ReturnType<typeof unsplash.photos.get>>["response"];
+
+const mapphoto = (r: Photo) => ({
+  id: r.id,
+  thumbnail: r.urls.thumb,
+  url: r.urls.regular,
+  full: r.urls.full,
+  raw: r.urls.raw,
+  alt: r.alt_description,
+  color: r.color,
+  width: r.width,
+  height: r.height,
+  author: {
+    username: r.user.username,
+    name: r.user.name,
+  },
+});
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,35 +30,38 @@ export default async function handler(
   const q = req.query.q as string;
   const page = req.query.page as string;
 
-  const { response } = await unsplash.search.getPhotos({
-    query: q,
-    orderBy: "relevant",
-    page: parseInt(page) ?? 1,
-  });
+  let images = [];
+  let total_pages = 0;
+  let n = 0;
 
-  const images = response.results.map((r) => ({
-    id: r.id,
-    thumbnail: r.urls.thumb,
-    url: r.urls.regular,
-    full: r.urls.full,
-    raw: r.urls.raw,
-    alt: r.alt_description,
-    color: r.color,
-    width: r.width,
-    height: r.height,
-    author: {
-      username: r.user.username,
-      name: r.user.name,
-    },
-  }));
+  if (!q.trim().length) {
+    const __n = 20;
+    // if no query, get radom images.
+    const { response } = await unsplash.photos.getRandom({
+      count: __n,
+    });
+    images = (response as Array<any>).map(mapphoto);
+    total_pages = 1;
+    n = __n;
+  } else {
+    const { response } = await unsplash.search.getPhotos({
+      query: q,
+      orderBy: "relevant",
+      page: parseInt(page) ?? 1,
+    });
+
+    images = response.results.map(mapphoto);
+    total_pages = response.total_pages;
+    n = response.total;
+  }
 
   switch (req.method) {
     case "GET":
       res.status(200).json({
         q: q,
-        pages: response.total_pages,
+        pages: total_pages,
         images: images,
-        n: response.total,
+        n: n,
       });
       break;
   }
