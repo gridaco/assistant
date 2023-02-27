5f3fd825a647f391d62cc389374e85607765b2c0
softmarshmallow <woojoo@softmarshmallow.com>
2023-02-27 13:28:09 +0900

    improve image prompting for non english languages

--- web/pages/api/generative/images.ts
+++ web/pages/api/generative/images.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
+import { translateForTextToImage } from "service/translation/translate-for-text-to-image";
 
 const OPENAI_API_SECRET_KEY = process.env.OPENAI_API_SECRET_KEY;
 const configuration = new Configuration({
@@ -15,8 +16,10 @@ export default async function handler(
   const q = req.query.q as string;
   const n = 2;
 
+  const prompt = await translateForTextToImage(q);
+
   const { data } = await openai.createImage({
-    prompt: q,
+    prompt: prompt,
     n: n,
     size: "1024x1024",
   });
