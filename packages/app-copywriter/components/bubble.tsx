ee89fd95a5f019369b7cfac143303df76c1c4675
Universe <woojoo@softmarshmallow.com>
2023-03-06 13:12:29 +0900

    made bubble stylable

--- packages/app-copywriter/components/bubble.tsx
+++ packages/app-copywriter/components/bubble.tsx
@@ -4,10 +4,16 @@ import styled from "@emotion/styled";
 export function Bubble({
   onClick,
   children,
+  style = {},
 }: React.PropsWithChildren<{
   onClick?: () => void;
+  style?: React.CSSProperties;
 }>) {
-  return <BubbleWrapper onClick={onClick}>{children}</BubbleWrapper>;
+  return (
+    <BubbleWrapper style={style} onClick={onClick}>
+      {children}
+    </BubbleWrapper>
+  );
 }
 
 const BubbleWrapper = styled.div`
@@ -23,4 +29,10 @@ const BubbleWrapper = styled.div`
     font-size: 16px;
     color: rgba(0, 0, 0, 0.8);
   }
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.08);
+  }
+
+  transition: all 0.05s ease-in-out;
 `;
